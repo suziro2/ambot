b9459d2255bbcab83d830fab55f6dacb244a6b05
suziro2 <suziro72@gmail.com>
2024-10-03 09:01:47 +0800

    first commit

---
+++ task1.py
@@ -0,0 +1,9 @@
+fav_fruits = input("How many fruits you like: ")
+
+my_fruits = []
+
+for i in fav_fruits:
+    fruits = input("Enter fruits:")
+    my_fruits.append(fruits)
+
+print(my_fruits)    
---
+++ task2.py
@@ -0,0 +1,14 @@
+list_of_fruits = input("list of fruits:")
+
+
+
+if list_of_fruits == "banana":
+    print( "hellow banana")
+
+elif list_of_fruits == "apple":
+    print("hellown apple")
+
+else:
+    print("No")
+
+
